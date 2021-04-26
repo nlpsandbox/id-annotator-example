75a6dd10fc210a9e37244650347f78f4026a4c65
Yao Yan <yy6linda@gmail.com>
2021-04-26 15:58:13 -0700

    add id annotator

---
+++ server/openapi_server/controllers/text_id_annotation_controller.py
@@ -0,0 +1,24 @@
+import connexion
+import six
+
+from openapi_server.models.error import Error  # noqa: E501
+from openapi_server.models.text_id_annotation_request import TextIdAnnotationRequest  # noqa: E501
+from openapi_server.models.text_id_annotation_response import TextIdAnnotationResponse  # noqa: E501
+from openapi_server import util
+
+
+def create_text_id_annotations(text_id_annotation_request=None):  # noqa: E501
+    """Annotate IDs in a clinical note
+
+    Return the ID annotations found in a clinical note # noqa: E501
+
+    :param text_id_annotation_request: 
+    :type text_id_annotation_request: dict | bytes
+
+    :rtype: TextIdAnnotationResponse
+    """
+    if connexion.request.is_json:
+        annotation_request = TextIdAnnotationRequest.from_dict(connexion.request.get_json())  # noqa: E501
+        note = annotation_request._note
+        print(note)
+    return 'do some magic!'
--- server/openapi_server/controllers/tool_controller.py
+++ server/openapi_server/controllers/tool_controller.py
@@ -1,6 +1,10 @@
+import connexion
+import six
+
+from openapi_server.models.error import Error  # noqa: E501
 from openapi_server.models.tool import Tool  # noqa: E501
 from openapi_server.models.tool_dependencies import ToolDependencies  # noqa: E501
-from openapi_server.models.license import License
+from openapi_server import util
 
 
 def get_tool():  # noqa: E501
@@ -11,19 +15,7 @@ def get_tool():  # noqa: E501
 
     :rtype: Tool
     """
-    tool = Tool(
-        name="date-annotator-example",
-        version="1.0.2",
-        license=License.APACHE_2_0,
-        repository="github:nlpsandbox/date-annotator-example",
-        description="Example implementation of the NLP Sandbox Date Annotator",
-        author="The NLP Sandbox Team",
-        author_email="thomas.schaffter@sagebionetworks.org",
-        url="https://github.com/nlpsandbox/date-annotator-example",
-        type="nlpsandbox:date-annotator",
-        api_version="1.0.2"
-    )
-    return tool, 200
+    return 'do some magic!'
 
 
 def get_tool_dependencies():  # noqa: E501
@@ -34,4 +26,4 @@ def get_tool_dependencies():  # noqa: E501
 
     :rtype: ToolDependencies
     """
-    return ToolDependencies(tool_dependencies=[]), 200
+    return 'do some magic!'
