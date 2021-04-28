d57fba916783a4bb7742f46a58a15bbaa5ac972f
yy6linda <39251215+yy6linda@users.noreply.github.com>
2021-04-27 23:17:35 -0700

    Add ID annotator (#5)
    * add id annotator
    
    * add id annotator
    
    * add id annotation tool
    
    * add the id annotation tool
    
    * add the id annotation tool
    
    * update README
    
    * delete libraries
    
    * update docker-compose.yml
    
    * Update server/openapi_server/__main__.py
    
    Co-authored-by: Thomas Schaffter <thomas.schaffter@gmail.com>
    
    * Update README.md
    
    Co-authored-by: Thomas Schaffter <thomas.schaffter@gmail.com>

--- server/openapi_server/models/__init__.py
+++ server/openapi_server/models/__init__.py
@@ -8,9 +8,9 @@ from openapi_server.models.health_check import HealthCheck
 from openapi_server.models.license import License
 from openapi_server.models.note import Note
 from openapi_server.models.text_annotation import TextAnnotation
-from openapi_server.models.text_date_annotation import TextDateAnnotation
-from openapi_server.models.text_date_annotation_all_of import TextDateAnnotationAllOf
-from openapi_server.models.text_date_annotation_request import TextDateAnnotationRequest
-from openapi_server.models.text_date_annotation_response import TextDateAnnotationResponse
+from openapi_server.models.text_id_annotation import TextIdAnnotation
+from openapi_server.models.text_id_annotation_all_of import TextIdAnnotationAllOf
+from openapi_server.models.text_id_annotation_request import TextIdAnnotationRequest
+from openapi_server.models.text_id_annotation_response import TextIdAnnotationResponse
 from openapi_server.models.tool import Tool
 from openapi_server.models.tool_dependencies import ToolDependencies
