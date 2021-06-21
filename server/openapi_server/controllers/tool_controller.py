b098e599a682046986b5f907d815ff57af08c1fa
Thomas Schaffter <thomas.schaffter@gmail.com>
2021-06-20 21:36:31 -0700

    Update to schemas 1.2.0 (#20)
    * Run npm run generate:server:latest
    
    * Update schemas and annotator version
    
    * Update npm scripts
    
    * Update tool description

--- server/openapi_server/controllers/tool_controller.py
+++ server/openapi_server/controllers/tool_controller.py
@@ -1,5 +1,6 @@
 from openapi_server.models.tool import Tool  # noqa: E501
 from openapi_server.models.tool_dependencies import ToolDependencies  # noqa: E501
+from openapi_server.models.tool_type import ToolType
 from openapi_server.models.license import License
 
 
@@ -13,15 +14,16 @@ def get_tool():  # noqa: E501
     """
     tool = Tool(
         name="id-annotator-example",
-        version="1.1.2",
+        version="1.2.0",
         license=License.APACHE_2_0,
         repository="github:nlpsandbox/id-annotator-example",
-        description="Example implementation of the NLP Sandbox ID Annotator",
+        description="Example implementation of the NLP Sandbox ID Annotator " +
+                " API",
         author="NLP Sandbox Team",
         author_email="team@nlpsandbox.io",
         url="https://github.com/nlpsandbox/id-annotator-example",
-        type="nlpsandbox:id-annotator",
-        api_version="1.1.2"
+        type=ToolType.ID_ANNOTATOR,
+        api_version="1.2.0"
     )
     return tool, 200
 
