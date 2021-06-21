9eb65904fdf05273192c76c11ab0e5c074887f51
tschaffter <thomas.schaffter@gmail.com>
2021-06-20 22:13:06 -0700

    Fix links in README

--- server/openapi_server/controllers/tool_controller.py
+++ server/openapi_server/controllers/tool_controller.py
@@ -18,7 +18,7 @@ def get_tool():  # noqa: E501
         license=License.APACHE_2_0,
         repository="github:nlpsandbox/id-annotator-example",
         description="Example implementation of the NLP Sandbox ID Annotator " +
-                " API",
+                "API",
         author="NLP Sandbox Team",
         author_email="team@nlpsandbox.io",
         url="https://github.com/nlpsandbox/id-annotator-example",
